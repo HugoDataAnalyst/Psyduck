e503909914da5698b111be82e212b9b0c3f28c3c
hugo gomes <gothugo21@gmail.com>
2024-01-03 15:35:44 +0000

    add celery parameters

--- celery_app.py
+++ celery_app.py
@@ -4,3 +4,5 @@ from app_config import app_config
 celery = Celery(__name__, broker=app_config.celery_broker_url, backend=app_config.celery_result_backend)
 
 celery.conf.broker_connection_retry_on_startup = True
+celery.conf.task_acks_late = True
+celery.conf.worker_reject_on_worker_lost = True
