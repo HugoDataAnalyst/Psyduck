6663f6e3e9943f7540258574bb6fcdbd9b82b1c1
hugo gomes <gothugo21@gmail.com>
2024-01-04 00:06:09 +0000

    typos..

--- SQL/create_db_tables.py
+++ SQL/create_db_tables.py
@@ -46,8 +46,8 @@ CREATE TABLE IF NOT EXISTS api_pokemon_area_stats (
 	total_top1_ultra INTEGER,
 	total_shiny INTEGER,
 	area_name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-	avg_despawn VARCHAR(15)
-    PRIMARY KEY (pokemon_id, form, area_name)	
+	avg_despawn VARCHAR(15),
+    PRIMARY KEY (pokemon_id, form, area_name)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;	
 '''
 
