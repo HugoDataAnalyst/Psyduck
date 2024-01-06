acdf4e12cfaaca4de5e5c509409eb25060e6ec2d
hugo gomes <gothugo21@gmail.com>
2024-01-06 23:19:39 +0000

    pathing

--- start_webhookparser.py
+++ start_webhookparser.py
@@ -1,5 +1,5 @@
 import uvicorn
-from app_config import app_config
+from config.app_config import app_config
 
 if __name__ == "__main__":
-    uvicorn.run("webhookparser:webhook_processor", host=app_config.receiver_host, port=app_config.receiver_port, reload=app_config.receiver_reload)
+    uvicorn.run("receiver.webhookparser:webhook_processor", host=app_config.receiver_host, port=app_config.receiver_port, reload=app_config.receiver_reload)
