3148ff76f4d850995b9f4f6f76f91ba87310dbd2
hugo gomes <gothugo21@gmail.com>
2024-01-10 16:31:24 +0000

    SQL

--- SQL/create_database_schema.py
+++ SQL/create_database_schema.py
@@ -78,6 +78,7 @@ CREATE TABLE IF NOT EXISTS daily_total_storage_pokemon_stats (
 # Daily API grouped
 create_daily_api_pokemon_stats_sql = '''
 CREATE TABLE IF NOT EXISTS daily_api_pokemon_stats (
+	day DATE,
 	pokemon_id INTEGER NOT NULL,
 	form VARCHAR(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
 	avg_lat FLOAT,
@@ -97,6 +98,7 @@ CREATE TABLE IF NOT EXISTS daily_api_pokemon_stats (
 # Weekly API grouped
 create_weekly_api_pokemon_stats_table_sql = '''
 CREATE TABLE IF NOT EXISTS weekly_api_pokemon_stats (
+	day DATE,
 	pokemon_id INTEGER NOT NULL,
 	form VARCHAR(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
 	avg_lat FLOAT,
@@ -116,6 +118,7 @@ CREATE TABLE IF NOT EXISTS weekly_api_pokemon_stats (
 # Monthly API grouped
 create_monthly_api_pokemon_stats_table_sql = '''
 CREATE TABLE IF NOT EXISTS monthly_api_pokemon_stats (
+	day DATE,
 	pokemon_id INTEGER NOT NULL,
 	form VARCHAR(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
 	avg_lat FLOAT,
@@ -198,6 +201,60 @@ BEGIN
   END WHILE;
 END;
 '''
+
+# PROCEDURE for hourly_total_updates
+create_procedure_update_hourly_total_stats = f'''
+CREATE PROCEDURE update_hourly_total_stats()
+BEGIN
+    CREATE TEMPORARY TABLE IF NOT EXISTS temp_hourly_total_stats AS
+    SELECT *
+    FROM pokemon_sightings
+    WHERE inserted_at >= NOW() - INTERVAL 60 MINUTE;
+
+    -- Creating a temporary table with all area names
+    CREATE TEMPORARY TABLE IF NOT EXISTS all_area_names AS
+    SELECT DISTINCT area_name
+    FROM pokemon_sightings;
+
+    REPLACE INTO hourly_total_api_pokemon_stats
+    SELECT
+        a.area_name,
+        COALESCE(t.total, 0) AS total,
+        COALESCE(t.total_iv100, NULL) AS total_iv100,
+        COALESCE(t.total_iv0, NULL) AS total_iv0,
+        COALESCE(t.total_top1_little, NULL) AS total_top1_little,
+        COALESCE(t.total_top1_great, NULL) AS total_top1_great,
+        COALESCE(t.total_top1_ultra, NULL) AS total_top1_ultra,
+        COALESCE(t.total_shiny, NULL) AS total_shiny,
+        COALESCE(t.avg_despawn, NULL) AS avg_despawn
+    FROM all_area_names a
+    LEFT JOIN (
+        SELECT
+            area_name,
+            COUNT(pokemon_id) AS total,
+            SUM(CASE WHEN iv = 100 THEN 1 ELSE NULL END) AS total_iv100,
+            SUM(CASE WHEN iv = 0 THEN 1 ELSE NULL END) AS total_iv0,
+            SUM(CASE WHEN pvp_little_rank = 1 THEN 1 ELSE NULL END) AS total_top1_little,
+            SUM(CASE WHEN pvp_great_rank = 1 THEN 1 ELSE NULL END) AS total_top1_great,
+            SUM(CASE WHEN pvp_ultra_rank = 1 THEN 1 ELSE NULL END) AS total_top1_ultra,
+            SUM(CASE WHEN shiny = 1 THEN 1 ELSE NULL END) AS total_shiny,
+            AVG(despawn_time) AS avg_despawn
+        FROM temp_hourly_total_stats
+        GROUP BY area_name
+    ) t ON a.area_name = t.area_name;
+
+    DROP TEMPORARY TABLE IF EXISTS temp_hourly_total_stats;
+    DROP TEMPORARY TABLE IF EXISTS all_area_names;
+END;
+'''
+# EVENT to call procedure update_hourly_total_stats()
+create_event_update_hourly_total_stats_sql = f'''
+CREATE EVENT IF NOT EXISTS event_update_hourly_total_stats
+ON SCHEDULE EVERY 1 HOUR
+STARTS (TIMESTAMP(CURRENT_DATE) + INTERVAL 1 HOUR)
+DO
+CALL update_hourly_total_stats();
+'''
 # EVENT Cleaner using delete_pokemon_sightings_batches
 create_event_clean_pokemon_sightings = f'''
 CREATE EVENT IF NOT EXISTS clean_pokemon_sightings
@@ -277,8 +334,13 @@ CREATE EVENT IF NOT EXISTS event_update_api_daily_stats
 ON SCHEDULE EVERY 1 DAY
 STARTS ADDDATE(ADDDATE(CURDATE(), INTERVAL 1 DAY), INTERVAL 1 HOUR)
 DO
-    REPLACE INTO daily_api_pokemon_stats
+BEGIN
+    DELETE FROM daily_api_pokemon_stats
+    WHERE day = CURDATE() - INTERVAL 1 DAY;
+
+    INSERT INTO daily_api_pokemon_stats (day, pokemon_id, form, avg_lat, avg_lon, total, total_iv100, total_iv0, total_top1_little, total_top1_great, total_top1_ultra, total_shiny, area_name, avg_despawn)
     SELECT
+        CURDATE() - INTERVAL 1 DAY as day,
         pokemon_id,
         form,
         avg_lat,
@@ -294,17 +356,22 @@ DO
         avg_despawn
     FROM grouped_total_daily_pokemon_stats
     WHERE day = CURDATE() - INTERVAL 1 DAY
-    GROUP BY pokemon_id, form, area_name
     ORDER BY area_name, pokemon_id;
+END;
 '''
-# EVENT grouped Weekly API
+# EVENT grouped Weekly API -- rework needed
 create_event_update_api_weekly_stats_sql = f'''
 CREATE EVENT IF NOT EXISTS event_update_api_weekly_stats
 ON SCHEDULE EVERY 1 WEEK
 STARTS ADDDATE(ADDDATE(CURDATE(), INTERVAL 7 DAY), INTERVAL '1:15' HOUR_MINUTE)
 DO
-    REPLACE INTO weekly_api_pokemon_stats
+BEGIN
+	DELETE FROM weekly_api_pokemon_stats
+	WHERE day = CURDATE() - INTERVAL 7 DAY
+
+    INSERT INTO weekly_api_pokemon_stats (day, pokemon_id, form, avg_lat, avg_lon, total, total_iv100, total_iv0, total_top1_little, total_top1_great, total_top1_ultra, total_shiny, area_name, avg_despawn)
     SELECT
+    	CURDATE() - INTERVAL 7 DAY AS day,
         pokemon_id,
         form,
         avg_lat,
@@ -320,17 +387,22 @@ DO
         avg_despawn
     FROM grouped_total_daily_pokemon_stats
     WHERE day = CURDATE() - INTERVAL 7 DAY
-    GROUP BY pokemon_id, form, area_name
     ORDER BY area_name, pokemon_id;
+END;
 '''
-# EVENT grouped Monthly API
+# EVENT grouped Monthly API -- rework needed
 create_event_update_api_monthly_stats_sql = f'''
 CREATE EVENT IF NOT EXISTS event_update_api_monthly_stats
 ON SCHEDULE EVERY 1 MONTH
 STARTS ADDDATE(ADDDATE(CURDATE(), INTERVAL 1 MONTH), INTERVAL '2:10' HOUR_MINUTE)
 DO
-    REPLACE INTO monthly_api_pokemon_stats
+BEGIN
+	DELETE FROM monthly_api_pokemon_stats
+	WHERE day = CURDATE() - INTERVAL 1 MONTH
+
+    INSERT INTO monthly_api_pokemon_stats(day, pokemon_id, form, avg_lat, avg_lon, total, total_iv100, total_iv0, total_top1_little, total_top1_great, total_top1_ultra, total_shiny, area_name, avg_despawn)
     SELECT
+    	CURTDATE() - INTERVAL 1 MONTH as day,
         pokemon_id,
         form,
         avg_lat,
@@ -346,37 +418,7 @@ DO
         avg_despawn
     FROM grouped_total_daily_pokemon_stats
     WHERE day = CURDATE() - INTERVAL 1 MONTH
-    GROUP BY pokemon_id, form, area_name
     ORDER BY area_name, pokemon_id;
-'''
-# EVENT total Hourly API
-create_event_update_hourly_total_stats_sql = f'''
-CREATE EVENT IF NOT EXISTS event_update_hourly_total_stats
-ON SCHEDULE EVERY 1 HOUR
-STARTS (TIMESTAMP(CURRENT_DATE) + INTERVAL 1 HOUR)
-DO
-BEGIN
-	CREATE TEMPORARY TABLE IF NOT EXISTS temp_hourly_total_stats AS
-	SELECT *
-	FROM pokemon_sightings
-	WHERE inserted_at >= NOW() - INTERVAL 60 MINUTE;
-
-	REPLACE INTO hourly_total_api_pokemon_stats
-	SELECT
-		area_name,
-		COUNT(pokemon_id) AS total,
-		SUM(CASE WHEN iv = 100 THEN 1 ELSE NULL END) AS total_iv100,
-   		SUM(CASE WHEN iv = 0 THEN 1 ELSE NULL END) AS total_iv0,
-   		SUM(CASE WHEN pvp_little_rank = 1 THEN 1 ELSE NULL END) AS total_top1_little,
-		SUM(CASE WHEN pvp_great_rank = 1 THEN 1 ELSE NULL END) AS total_top1_great,
-   		SUM(CASE WHEN pvp_ultra_rank = 1 THEN 1 ELSE NULL END) AS total_top1_ultra,
-		SUM(CASE WHEN shiny = 1 THEN 1 ELSE NULL END) AS total_shiny,
-		AVG(despawn_time) AS avg_despawn
-	FROM temp_hourly_total_stats
-	GROUP BY area_name
-	ORDER BY area_name;
-
-	DROP TEMPORARY TABLE IF EXISTS temp_hourly_total_stats;
 END;
 '''
 # EVENT total Daily API
@@ -407,29 +449,28 @@ CREATE EVENT IF NOT EXISTS event_update_total_api_stats
 ON SCHEDULE EVERY 1 DAY
 STARTS ADDDATE(ADDDATE(CURDATE(), INTERVAL 1 DAY), INTERVAL '1:15' HOUR_MINUTE)
 DO
-    INSERT INTO total_api_pokemon_stats (area_name, total, total_iv100, total_iv0, total_top1_little, total_top1_great, total_top1_ultra, total_shiny, avg_despawn)
-    SELECT
-        area_name,
-        total,
-        total_iv100,
-        total_iv0,
-        total_top1_little,
-        total_top1_great,
-        total_top1_ultra,
-        total_shiny,
-        avg_despawn
-    FROM daily_total_storage_pokemon_stats
-    WHERE day = CURDATE() - INTERVAL 1 DAY
-    GROUP BY area_name
-    ON DUPLICATE KEY UPDATE
-    	total = total + VALUES(total),
-        total_iv100 = total_iv100 + VALUES(total_iv100),
-        total_iv0 = total_iv0 + VALUES(total_iv0),
-        total_top1_little = total_top1_little + VALUES(total_top1_little),
-        total_top1_great = total_top1_great + VALUES(total_top1_great),
-        total_top1_ultra = total_top1_ultra + VALUES(total_top1_ultra),
-        total_shiny = total_shiny + VALUES(total_shiny),
-        avg_despawn = (avg_despawn + VALUES(avg_despawn)) / 2;
+	INSERT INTO total_api_pokemon_stats (area_name, total, total_iv100, total_iv0, total_top1_little, total_top1_great, total_top1_ultra, total_shiny, avg_despawn)
+	SELECT
+    	d.area_name,
+    	d.total,
+    	d.total_iv100,
+    	d.total_iv0,
+    	d.total_top1_little,
+    	d.total_top1_great,
+    	d.total_top1_ultra,
+    	d.total_shiny,
+    	d.avg_despawn
+	FROM daily_total_storage_pokemon_stats d
+	WHERE d.day = CURDATE() - INTERVAL 1 DAY
+	ON DUPLICATE KEY UPDATE
+    	total = total_api_pokemon_stats.total + d.total,
+    	total_iv100 = total_api_pokemon_stats.total_iv100 + d.total_iv100,
+    	total_iv0 = total_api_pokemon_stats.total_iv0 + d.total_iv0,
+    	total_top1_little = total_api_pokemon_stats.total_top1_little + d.total_top1_little,
+    	total_top1_great = total_api_pokemon_stats.total_top1_great + d.total_top1_great,
+    	total_top1_ultra = total_api_pokemon_stats.total_top1_ultra + d.total_top1_ultra,
+    	total_shiny = total_api_pokemon_stats.total_shiny + d.total_shiny,
+    	avg_despawn = (total_api_pokemon_stats.avg_despawn + d.avg_despawn) / 2;
 '''
 
 def create_cursor(connection):
@@ -524,6 +565,8 @@ def create_database_schema():
 				print(f"Procedure {procedure_name} created.")
 
 		check_and_create_procedure(create_procedure_clean_pokemon_batches, 'delete_pokemon_sightings_batches')
+		handle_multiple_results(conn)
+		check_and_create_procedure(create_procedure_update_hourly_total_stats, 'update_hourly_total_stats')
 		handle_multiple_results(conn)
 
 		close_cursor(cursor)
