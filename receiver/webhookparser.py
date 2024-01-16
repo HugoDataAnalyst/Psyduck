ee972c6204c8b935c47585d46eb0e034f8d4afff
hugo gomes <gothugo21@gmail.com>
2024-01-16 22:39:19 +0000

    lock workers

--- receiver/webhookparser.py
+++ receiver/webhookparser.py
@@ -89,104 +89,106 @@ async def receive_data(request: Request):
     logger.debug(f"Received request on path: {request.url.path}")
     global data_queue, is_processing_queue
     await validate_remote_addr(request)
-    logger.info(f"Queue size before processing: {len(data_queue)}")
-    with data_queue_lock: 
-        data = await request.json()
- 
+
     if not geofences:
         logger.info("No geofences matched.")
         return {"status": "info", "message": "No geofences available"}
 
-    def filter_criteria(message):
-        required_fields = [
-            'pokemon_id', 'form', 'latitude', 'longitude',
-            'individual_attack', 'individual_defense', 'individual_stamina'
-        ]
-        return all(message.get(field) is not None for field in required_fields)
+    with data_queue_lock: 
+        logger.info(f"Queue size before processing: {len(data_queue)}")
+        data = await request.json()
 
-    def extract_pvp_ranks(pvp_data):
-        ranks = {f'pvp_{category}_rank': None for category in ['great', 'little', 'ultra']}
-        if pvp_data:
-            for category in ['great', 'little', 'ultra']:
-                category_data = pvp_data.get(category, [])
-                ranks[f'pvp_{category}_rank'] = 1 if any(entry.get('rank') == 1 for entry in category_data) else None
-        return ranks
+        def filter_criteria(message):
+            required_fields = [
+                'pokemon_id', 'form', 'latitude', 'longitude',
+                'individual_attack', 'individual_defense', 'individual_stamina'
+            ]
+            return all(message.get(field) is not None for field in required_fields)
 
-    if isinstance(data, list):
-        for item in data:
-            if item.get('type') == 'pokemon':
-                message = item.get('message', {})
-                if filter_criteria(message):
-                    lat, lon = message.get('latitude'), message.get('longitude')
-                    inside, geofence_name = is_inside_geofence(lat, lon, geofences)
-                    if inside:
-                        ind_attack = message['individual_attack']
-                        ind_defense = message['individual_defense']
-                        ind_stamina = message['individual_stamina']
+        def extract_pvp_ranks(pvp_data):
+            ranks = {f'pvp_{category}_rank': None for category in ['great', 'little', 'ultra']}
+            if pvp_data:
+                for category in ['great', 'little', 'ultra']:
+                    category_data = pvp_data.get(category, [])
+                    ranks[f'pvp_{category}_rank'] = 1 if any(entry.get('rank') == 1 for entry in category_data) else None
+            return ranks
 
-                        if ind_attack == ind_defense == ind_stamina == 15:
-                            iv_value = 100
-                        elif ind_attack == ind_defense == ind_stamina == 0:
-                            iv_value = 0
-                        else:
-                            iv_value = None
+        if isinstance(data, list):
+            for item in data:
+                if item.get('type') == 'pokemon':
+                    message = item.get('message', {})
+                    if filter_criteria(message):
+                        lat, lon = message.get('latitude'), message.get('longitude')
+                        inside, geofence_name = is_inside_geofence(lat, lon, geofences)
+                        if inside:
+                            ind_attack = message['individual_attack']
+                            ind_defense = message['individual_defense']
+                            ind_stamina = message['individual_stamina']
 
-                        despawn_time = calculate_despawn_time(
-                            message.get('disappear_time'),
-                            message.get('first_seen')
-                        )
-                        filtered_data = {
-                            'pokemon_id': message['pokemon_id'],
-                            'form': message['form'],
-                            'latitude': lat,
-                            'longitude': lon,
-                            'iv': iv_value,
-                            **extract_pvp_ranks(message.get('pvp', {})),
-                            'shiny':message['shiny'],
-                            'area_name': geofence_name,
-                            'despawn_time': despawn_time
-                        }
+                            if ind_attack == ind_defense == ind_stamina == 15:
+                                iv_value = 100
+                            elif ind_attack == ind_defense == ind_stamina == 0:
+                                iv_value = 0
+                            else:
+                                iv_value = None
 
-                        item_unique_id = generate_unique_id(filtered_data)
-                        data_queue.append((filtered_data, item_unique_id))
+                            despawn_time = calculate_despawn_time(
+                                message.get('disappear_time'),
+                                message.get('first_seen')
+                            )
+                            filtered_data = {
+                                'pokemon_id': message['pokemon_id'],
+                                'form': message['form'],
+                                'latitude': lat,
+                                'longitude': lon,
+                                'iv': iv_value,
+                                **extract_pvp_ranks(message.get('pvp', {})),
+                                'shiny':message['shiny'],
+                                'area_name': geofence_name,
+                                'despawn_time': despawn_time
+                            }
 
-                        if len(data_queue) >= app_config.max_queue_size and not is_processing_queue :
-                            is_processing_queue = True
-                            process_full_queue()
+                            item_unique_id = generate_unique_id(filtered_data)
+                            data_queue.append((filtered_data, item_unique_id))
+
+                            if len(data_queue) >= app_config.max_queue_size and not is_processing_queue :
+                                is_processing_queue = True
+                                process_full_queue()
+                            logger.info(f"Queue size AFTER processing: {len(data_queue)}")
+                    else:
+                        logger.debug("Data did not meet filter criteria")
                 else:
-                    logger.debug("Data did not meet filter criteria")
-            else:
-                logger.debug(f"Unsupported data type found in payload: {item.get('type')}")
-    else:
-        logger.error("Received data is not in list format")
+                    logger.debug(f"Unsupported data type found in payload: {item.get('type')}")
+        else:
+            logger.error("Received data is not in list format")
 
-    return {"status": "success"}
-    logger.info(f"Queue size AFTER processing: {len(data_queue)}")
+        return {"status": "success"}
 
 def process_full_queue():
     global data_queue, is_processing_queue
-    logger.info(f"Starting full queue processing. Current queue size: {len(data_queue)}")
+    with data_queue_lock:
+        logger.info(f"Starting full queue processing. Current queue size: {len(data_queue)}")
+        retry_count = 0
 
-    retry_count = 0
-    while retry_count <= app_config.max_retries:
-        try:
-            current_batch_data = [item[0] for item in data_queue[:app_config.max_queue_size]]
-            current_batch_ids = [item[1] for item in data_queue[:app_config.max_queue_size]]
-            batch_unique_id = generate_unique_id(current_batch_ids)
+        while retry_count <= app_config.max_retries:
+            try:
+                current_batch_data = [item[0] for item in data_queue[:app_config.max_queue_size]]
+                current_batch_ids = [item[1] for item in data_queue[:app_config.max_queue_size]]
+                batch_unique_id = generate_unique_id(current_batch_ids)
 
-            insert_data_task.delay(current_batch_data, batch_unique_id)
-            logger.info(f"Processed full queue with unique_id: {batch_unique_id}")
-            data_queue = data_queue[app_config.max_queue_size:]
-            break
-        except Exception as e:
-            retry_count += 1
-            logger.error(f"Error processing queue on attempt {retry_count}: {e}")
-            time.sleep(app_config.retry_delay)
+                insert_data_task.delay(current_batch_data, batch_unique_id)
+                logger.info(f"Processed full queue with unique_id: {batch_unique_id}")
+                data_queue = data_queue[app_config.max_queue_size:]
+                break
+            except Exception as e:
+                retry_count += 1
+                logger.error(f"Error processing queue on attempt {retry_count}: {e}")
+                time.sleep(app_config.retry_delay)
     
-    if retry_count > app_config.max_retries:
-        logger.error("Maximum retry attempts reached. Unable to process queue")
-    is_processing_queue = False
-    logger.info(f"Finished queue processing. Updated queue size: {len(data_queue)}")
+        if retry_count > app_config.max_retries:
+            logger.error("Maximum retry attempts reached. Unable to process queue")
+        is_processing_queue = False
+        logger.info(f"Finished queue processing. Updated queue size: {len(data_queue)}")
 
 def process_remaining_queue_on_shutdown():
     global data_queue
