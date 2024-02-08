a9b8f6fa993c649b1f89569ff4e73a17de57bc65
hugo gomes <gothugo21@gmail.com>
2024-02-08 00:07:13 +0000

    003_migration.sql

--- migrations/003_migration.sql
+++ migrations/003_migration.sql
@@ -1,4 +1,22 @@
 -- Simple migration to call the dynamic hourly updates
-CALL CreateOrUpdateHourlySurgeEvent();
+CREATE EVENT IF NOT EXISTS call_CreateOrUpdateHourlySurgeEvent
+ON SCHEDULE AT CURRENT_TIMESTAMP
+ON COMPLETION PRESERVE
+DO
+BEGIN
+  CALL CreateOrUpdateHourlySurgeEvent();
+END;
 
-CALL CreateOrUpdateHourlyTotalEvent();
+CREATE EVENT IF NOT EXISTS call_CreateOrUpdateHourlyTotalEvent
+ON SCHEDULE AT CURRENT_TIMESTAMP
+ON COMPLETION PRESERVE
+DO
+BEGIN
+  CALL CreateOrUpdateHourlyTotalEvent();
+END;
+
+DROP EVENT IF EXISTS call_CreateOrUpdateHourlySurgeEvent;
+DROP EVENT IF EXISTS call_CreateOrUpdateHourlyTotalEvent;
+
+DROP PROCEDURE IF EXISTS CreateOrUpdateHourlySurgeEvent;
+DROP PROCEDURE IF EXISTS CreateOrUpdateHourlyTotalEvent;
