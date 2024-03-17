62ef84c43cabe7f6574ef5245c256d5f61c68e0b
hugo gomes <gothugo21@gmail.com>
2024-03-17 15:50:48 +0000

    fix pebcak

---
+++ migrations/009_migration.sql
@@ -0,0 +1,54 @@
+-- fix pebcak
+-- Pokemon Section
+DROP PROCEDURE IF EXISTS delete_pokemon_sightings_batches;
+CREATE PROCEDURE delete_pokemon_sightings_batches()
+BEGIN
+  DECLARE done INT DEFAULT FALSE;
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+  WHILE NOT done DO
+    DELETE FROM pokemon_sightings
+    WHERE inserted_at < CONCAT(CURDATE() - INTERVAL 1 DAY, ' 21:00:00')
+    LIMIT 20000;
+
+    IF (ROW_COUNT() = 0) THEN
+      SET done = TRUE;
+    END IF;
+  END WHILE;
+END;
+
+-- Raid Section
+DROP PROCEDURE IF EXISTS delete_raid_sightings_batches;
+CREATE PROCEDURE delete_raid_sightings_batches()
+BEGIN
+  DECLARE done INT DEFAULT FALSE;
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+  WHILE NOT done DO
+    DELETE FROM raid_sightings
+    WHERE inserted_at < CONCAT(CURDATE() - INTERVAL 1 DAY, ' 21:00:00')
+    LIMIT 20000;
+
+    IF (ROW_COUNT() = 0) THEN
+      SET done = TRUE;
+    END IF;
+  END WHILE;
+END;
+
+-- Invasion Section
+DROP PROCEDURE IF EXISTS delete_invasion_sightings_batches;
+CREATE PROCEDURE delete_invasion_sightings_batches()
+BEGIN
+  DECLARE done INT DEFAULT FALSE;
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+  WHILE NOT done DO
+    DELETE FROM invasion_sightings
+    WHERE inserted_at < CONCAT(CURDATE() - INTERVAL 1 DAY, ' 21:00:00')
+    LIMIT 20000;
+
+    IF (ROW_COUNT() = 0) THEN
+      SET done = TRUE;
+    END IF;
+  END WHILE;
+END;
