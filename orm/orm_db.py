da4f82db84b6c2a8ed26ca95c76c1edadcb3fa43
hugo gomes <gothugo21@gmail.com>
2024-05-25 23:21:13 +0100

    orm connection

--- orm/orm_db.py
+++ orm/orm_db.py
@@ -1,11 +1,33 @@
 from tortoise import Tortoise
+from tortoise.contrib.fastapi import register_tortoise
 from config.app_config import app_config
 
+db_config = {
+    'connections': {
+        'default': {
+            'engine': 'tortoise.backends.mysql',
+            'credentials': {
+                'host': app_config.db_host,
+                'port': app_config.db_port,
+                'user': app_config.db_user,
+                'password': app_config.db_password,
+                'database': app_config.db_name,
+            }
+        }
+    },
+    'apps': {
+        'models': {
+            'models': ['orm.models'],
+            'default_connection': 'default',
+        }
+    }
+}
+
 async def init():
-    db_url = f"mysql://{app_config.db_user}:{app_config.db_password}@{app_config.db_host}:{app_config.db_port}/{app_config.db_name}"
-    await Tortoise.init(
-        db_url=db_url,
-        modules={'models': ['orm.models']}
+    register_tortoise(
+        None,  # You can pass an app instance here if you have one
+        config=db_config,
+        generate_schemas=False  # Set to True if you want to generate schemas automatically
     )
     # Can explore the code below to fully generate the database and its full schema on another time. Since it doesn't deal with versions by nature.
     # await Tortoise.generate_schemas()
