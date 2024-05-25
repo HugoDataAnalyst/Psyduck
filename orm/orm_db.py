d626c512450b354d456e518303f3a0b206e95490
hugo gomes <gothugo21@gmail.com>
2024-05-25 22:42:42 +0100

    Remove the schema generation from tortoise

--- orm/orm_db.py
+++ orm/orm_db.py
@@ -2,13 +2,13 @@ from tortoise import Tortoise, run_async
 from config.app_config import app_config
 
 async def init():
+    db_url = f"mysql://{app_config.db_user}:{app_config.db_password}@{app_config.db_host}:{app_config.db_port}/{app_config.db_name}"
     await Tortoise.init(
-        db_url='mysql://app_config.db_host:app_config.db_password@app_config.host:app_config.port/app_config.db_name',
+        db_url=db_url,
         modules={'models': ['orm.models']}
     )
-    await Tortoise.generate_schemas()
+    # Can explore the code below to fully generate the database and its full schema on another time. Since it doesn't deal with versions by nature.
+    # await Tortoise.generate_schemas()
 
 async def close():
     await Tortoise.close_connections()
-
-run_async(init())
