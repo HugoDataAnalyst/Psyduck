4994a8a30c05e142baf9aa702ed6536532b7fb3f
hugo gomes <gothugo21@gmail.com>
2024-05-28 17:52:12 +0100

    ah orm.... It needs to define all values, when not 1 single PK is assigned <.<

--- orm/queries.py
+++ orm/queries.py
@@ -76,85 +76,352 @@ class DatabaseOperations:
         return await model.all()
 
     async def fetch_daily_pokemon_grouped_stats(self):
-        return await self.fetch_all_records(DailyPokemonGroupedStats, order_by=["area_name", "pokemon_id"])
+        return await DailyPokemonGroupedStats.all().order_by("area_name", "pokemon_id").values(
+            "day",
+            "pokemon_id",
+            "form",
+            "avg_lat",
+            "avg_lon",
+            "total",
+            "total_iv100",
+            "total_iv0",
+            "total_top1_little",
+            "total_top1_great",
+            "total_top1_ultra",
+            "total_shiny",
+            "area_name",
+            "avg_despawn"
+        )
 
     async def fetch_weekly_pokemon_grouped_stats(self):
-        return await self.fetch_all_records(WeeklyPokemonGroupedStats, order_by=["area_name", "pokemon_id"])
+        return await WeeklyPokemonGroupedStats.all().order_by("area_name", "pokemon_id").values(
+            "day",
+            "pokemon_id",
+            "form",
+            "avg_lat",
+            "avg_lon",
+            "total",
+            "total_iv100",
+            "total_iv0",
+            "total_top1_little",
+            "total_top1_great",
+            "total_top1_ultra",
+            "total_shiny",
+            "area_name",
+            "avg_despawn"
+        )
 
     async def fetch_monthly_pokemon_grouped_stats(self):
-        return await self.fetch_all_records(MonthlyPokemonGroupedStats, order_by=["area_name", "pokemon_id"])
+        return await MonthlyPokemonGroupedStats.all().order_by("area_name", "pokemon_id").values(
+            "day",
+            "pokemon_id",
+            "form",
+            "avg_lat",
+            "avg_lon",
+            "total",
+            "total_iv100",
+            "total_iv0",
+            "total_top1_little",
+            "total_top1_great",
+            "total_top1_ultra",
+            "total_shiny",
+            "area_name",
+            "avg_despawn"
+        )
 
     async def fetch_hourly_pokemon_total_stats(self):
-        return await self.fetch_all_records(HourlyPokemonTotalStats, order_by=["area_name"])
+        return await HourlyPokemonTotalStats.all().order_by("area_name").values(
+            "area_name",
+            "total",
+            "total_iv100",
+            "total_iv0",
+            "total_top1_little",
+            "total_top1_great",
+            "total_top1_ultra",
+            "total_shiny",
+            "avg_despawn"
+        )
 
     async def fetch_daily_pokemon_total_stats(self):
-        return await self.fetch_all_records(DailyPokemonTotalStats, order_by=["area_name"])
+        return await DailyPokemonTotalStats.all().order_by("area_name").values(
+            "day",
+            "area_name",
+            "total",
+            "total_iv100",
+            "total_iv0",
+            "total_top1_little",
+            "total_top1_great",
+            "total_top1_ultra",
+            "total_shiny",
+            "avg_despawn"
+        )
 
     async def fetch_pokemon_total_stats(self):
-        return await self.fetch_all_records(PokemonTotalStats, order_by=["area_name"])
+        return await PokemonTotalStats.all().order_by("area_name").values(
+            "area_name",
+            "total",
+            "total_iv100",
+            "total_iv0",
+            "total_top1_little",
+            "total_top1_great",
+            "total_top1_ultra",
+            "total_shiny",
+            "avg_despawn"
+        )
 
     async def fetch_daily_surge_api_pokemon_stats(self):
-        return await self.fetch_all_records(DailySurgePokemonStats)
+        return await DailySurgePokemonStats.all().values(
+            "hour",
+            "total_iv100",
+            "total_iv0",
+            "total_top1_little",
+            "total_top1_great",
+            "total_top1_ultra",
+            "total_shiny"
+        )
 
     async def fetch_weekly_surge_api_pokemon_stats(self):
-        return await self.fetch_all_records(WeeklySurgePokemonStats)
+        return await WeeklySurgePokemonStats.all().values(
+            "hour",
+            "total_iv100",
+            "total_iv0",
+            "total_top1_little",
+            "total_top1_great",
+            "total_top1_ultra",
+            "total_shiny"
+        )
 
     async def fetch_monthly_surge_api_pokemon_stats(self):
-        return await self.fetch_all_records(MonthlySurgePokemonStats)
+        return await MonthlySurgePokemonStats.all().values(
+            "hour",
+            "total_iv100",
+            "total_iv0",
+            "total_top1_little",
+            "total_top1_great",
+            "total_top1_ultra",
+            "total_shiny"
+        )
 
     async def fetch_daily_quest_grouped_stats(self):
-        return await self.fetch_all_records(DailyQuestGroupedStats)
+        return await DailyQuestGroupedStats.all().values(
+            "day",
+            "area_name",
+            "ar_type",
+            "normal_type",
+            "reward_ar_type",
+            "reward_normal_type",
+            "reward_ar_item_id",
+            "reward_ar_item_amount",
+            "reward_normal_item_id",
+            "reward_normal_item_amount",
+            "reward_ar_poke_id",
+            "reward_ar_poke_form",
+            "reward_normal_poke_id",
+            "reward_normal_poke_form",
+            "total"
+        )
 
     async def fetch_weekly_quest_grouped_stats(self):
-        return await self.fetch_all_records(WeeklyQuestGroupedStats)
+        return await WeeklyQuestGroupedStats.all().values(
+            "day",
+            "area_name",
+            "ar_type",
+            "normal_type",
+            "reward_ar_type",
+            "reward_normal_type",
+            "reward_ar_item_id",
+            "reward_ar_item_amount",
+            "reward_normal_item_id",
+            "reward_normal_item_amount",
+            "reward_ar_poke_id",
+            "reward_ar_poke_form",
+            "reward_normal_poke_id",
+            "reward_normal_poke_form",
+            "total"
+        )
 
     async def fetch_monthly_quest_grouped_stats(self):
-        return await self.fetch_all_records(MonthlyQuestGroupedStats)
+        return await MonthlyQuestGroupedStats.all().values(
+            "day",
+            "area_name",
+            "ar_type",
+            "normal_type",
+            "reward_ar_type",
+            "reward_normal_type",
+            "reward_ar_item_id",
+            "reward_ar_item_amount",
+            "reward_normal_item_id",
+            "reward_normal_item_amount",
+            "reward_ar_poke_id",
+            "reward_ar_poke_form",
+            "reward_normal_poke_id",
+            "reward_normal_poke_form",
+            "total"
+        )
 
     async def fetch_daily_quest_total_stats(self):
-        return await self.fetch_all_records(DailyQuestTotalStats)
+        return await DailyQuestTotalStats.all().values(
+            "day",
+            "area_name",
+            "total_stops",
+            "ar",
+            "normal"
+        )
 
     async def fetch_total_quest_total_stats(self):
-        return await self.fetch_all_records(QuestTotalStats)
+        return await QuestTotalStats.all().values(
+            "area_name",
+            "ar",
+            "normal"
+        )
 
     async def fetch_daily_raid_grouped_stats(self):
-        return await self.fetch_all_records(DailyRaidGroupedStats)
+        return await DailyRaidGroupedStats.all().values(
+            "day",
+            "area_nmame",
+            "level",
+            "pokemon_id",
+            "form",
+            "costume",
+            "ex_raid_eligible",
+            "is_exclusive",
+            "total"
+        )
 
     async def fetch_weekly_raid_grouped_stats(self):
-        return await self.fetch_all_records(WeeklyRaidGroupedStats)
+        return await WeeklyRaidGroupedStats.all().values(
+            "day",
+            "area_nmame",
+            "level",
+            "pokemon_id",
+            "form",
+            "costume",
+            "ex_raid_eligible",
+            "is_exclusive",
+            "total"
+        )
 
     async def fetch_monthly_raid_grouped_stats(self):
-        return await self.fetch_all_records(MonthlyRaidGroupedStats)
+        return await MonthlyRaidGroupedStats.all().values(
+            "day",
+            "area_nmame",
+            "level",
+            "pokemon_id",
+            "form",
+            "costume",
+            "ex_raid_eligible",
+            "is_exclusive",
+            "total"
+        )
 
     async def fetch_hourly_raid_total_stats(self):
-        return await self.fetch_all_records(HourlyRaidTotalStats)
+        return await HourlyRaidTotalStats.all().values(
+            "area_name",
+            "total",
+            "total_ex_raid",
+            "total_exclusive"
+        )
 
     async def fetch_daily_raid_total_stats(self):
-        return await self.fetch_all_records(DailyRaidTotalStats)
+        return await DailyRaidTotalStats.all().values(
+            "day",
+            "area_name",
+            "total",
+            "total_ex_raid",
+            "total_exclusive"
+        )
 
     async def fetch_total_raid_total_stats(self):
-        return await self.fetch_all_records(RaidTotalStats)
+        return await RaidTotalStats.all().values(
+            "area_name",
+            "total",
+            "total_ex_raid",
+            "total_exclusive"
+        )
 
     async def fetch_daily_invasion_grouped_stats(self):
-        return await self.fetch_all_records(DailyInvasionGroupedStats)
+        return await DailyInvasionGroupedStats.all().values(
+            "day",
+            "area_name",
+            "display_type",
+            "grunt",
+            "total_grunts"
+        )
 
     async def fetch_weekly_invasion_grouped_stats(self):
-        return await self.fetch_all_records(WeeklyInvasionGroupedStats)
+        return await WeeklyInvasionGroupedStats.all().values(
+            "day",
+            "area_name",
+            "display_type",
+            "grunt",
+            "total_grunts"
+        )
 
     async def fetch_monthly_invasion_grouped_stats(self):
-        return await self.fetch_all_records(MonthlyInvasionGroupedStats)
+        return await MonthlyInvasionGroupedStats.all().values(
+            "day",
+            "area_name",
+            "display_type",
+            "grunt",
+            "total_grunts"
+        )
 
     async def fetch_hourly_invasion_total_stats(self):
-        return await self.fetch_all_records(HourlyInvasionTotalStats)
+        return await HourlyInvasionTotalStats.all().values(
+            "area_name",
+            "total_grunts",
+            "total_confirmed",
+            "total_unconfirmed"
+        )
 
     async def fetch_daily_invasion_total_stats(self):
-        return await self.fetch_all_records(DailyInvasionTotalStats)
+        return await DailyInvasionTotalStats.all().values(
+            "day",
+            "area_name",
+            "total_grunts",
+            "total_confirmed",
+            "total_unconfirmed"
+        )
 
     async def fetch_total_invasion_total_stats(self):
-        return await self.fetch_all_records(InvasionTotalStats)
+        return await InvasionTotalStats.all().values(
+            "area_name",
+            "total_grunts",
+            "total_confirmed",
+            "total_unconfirmed"
+        )
 
     async def fetch_hourly_pokemon_tth_stats(self):
-        return await self.fetch_all_records(HourlyPokemonTthStats)
+        return await HourlyPokemonTthStats.all().values(
+            "area_name",
+            "tth_5",
+            "tth_10",
+            "tth_15",
+            "tth_20",
+            "tth_25",
+            "tth_30",
+            "tth_35",
+            "tth_40",
+            "tth_45",
+            "tth_50",
+            "tth_55",
+            "tth_55_plus"
+        )
 
     async def fetch_daily_pokemon_tth_stats(self):
-        return await self.fetch_all_records(DailyPokemonTthStats)
+        return await DailyPokemonTthStats.all().values(
+            "hour",
+            "area_name",
+            "tth_5",
+            "tth_10",
+            "tth_15",
+            "tth_20",
+            "tth_25",
+            "tth_30",
+            "tth_35",
+            "tth_40",
+            "tth_45",
+            "tth_50",
+            "tth_55",
+            "tth_55_plus"
+        )
