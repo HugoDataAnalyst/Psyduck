8563511bda34ac28ecdf802c0f241af69975228a
hugo gomes <gothugo21@gmail.com>
2024-05-30 23:25:57 +0100

    create single events for each one, instead of one big event

--- orm/events.py
+++ orm/events.py
@@ -8,22 +8,12 @@ class EventGenerator:
         self.db_timezone_offset = app_config.db_timezone_offset
 
     async def get_unique_timezones(self):
-        return await self.db_ops.get_unique_timezones()
+        timezones = await self.db_ops.get_unique_timezones()
+        return [{'time_zone_offset': tz} for tz in timezones]
 
-    def generate_event_daily_sql(self, timezone_offset):
+    def generate_event_daily_sql(self, procedure_name, timezone_offset):
         offset_diff = timezone_offset - self.db_timezone_offset
-        event_name = f"event_{procedure_names}_{abs(offset_diff)}"
-        procedure_names = [
-            f"store_pokemon_total_{abs(offset_diff)}",
-            f"store_pokemon_grouped_{abs(offset_diff)}",
-            f"store_quest_total_{abs(offset_diff)}",
-            f"store_quest_grouped_{abs(offset_diff)}",
-            f"store_raid_total_{abs(offset_diff)}",
-            f"store_raid_grouped_{abs(offset_diff)}",
-            f"store_invasion_total_{abs(offset_diff)}",
-            f"store_invasion_grouped_{abs(offset_diff)}",
-            f"store_hourly_pokemon_tth"
-        ]
+        event_name = f"event_{procedure_name}_{abs(offset_diff)}"
         # Get the current UTC time and adjust it to the database's local time
         current_time_utc = datetime.utcnow()
         current_time_db = current_time_utc + timedelta(minutes=self.db_timezone_offset)
@@ -41,17 +31,29 @@ class EventGenerator:
         STARTS '{adjusted_event_time.strftime('%Y-%m-%d %H:%M:%S')}'
         DO
         BEGIN
-        {"; ".join([f"CALL {procedure}" for procedure in procedure_names])};
+        CALL {procedure_name};
         END;
         """
         return drop_event_sql, create_event_sql
 
     async def create_events(self):
         unique_timezones = await self.get_unique_timezones()
+        procedure_names = [
+            "store_pokemon_total",
+            "store_pokemon_grouped",
+            "store_quest_total",
+            "store_quest_grouped",
+            "store_raid_total",
+            "store_raid_grouped",
+            "store_invasion_total",
+            "store_invasion_grouped",
+            "store_hourly_pokemon_tth"
+        ]
         for timezone in unique_timezones:
             offset = timezone['time_zone_offset']
-            drop_event_sql, create_event_sql = self.generate_event_daily_sql(offset)
-            await self.db_ops.execute_sql(drop_event_sql)
-            await self.db_ops.execute_sql(create_event_sql)
-            print(f"Event for offset {offset} created/updated.")
+            for procedure in procedure_names:
+                drop_event_sql, create_event_sql = self.generate_event_daily_sql(f"{procedure}_{abs(offset)}", offset)
+                await self.db_ops.execute_sql(drop_event_sql)
+                await self.db_ops.execute_sql(create_event_sql)
+                print(f"Event for {procedure} with offset {offset} created/updated.")
 
